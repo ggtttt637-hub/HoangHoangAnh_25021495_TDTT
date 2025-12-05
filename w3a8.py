a24ecf25e09f55fd15131c6ff995e8c3992263db
ggtttt637-hub <ggtttt637@gmail.com>
2025-12-05 15:30:01 +0700

    Delete w3a8.py

--- w3a8.py
+++
@@ -1,8 +0,0 @@
-a = float(input("nhap chuoi a : "))
-b = float(input("nhap chuoi b : "))
-if len(a) > len(b):
-    print("chuoi a dai hon chuoi b")
-elif len(a) < len(b):
-    print("chuoi b dai hon chuoi a")
-else:
-    print("hai chuoi bang nhau")
