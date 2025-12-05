dfab9c20f1572334355cc4a4bed048fb785dc73d
ggtttt637-hub <ggtttt637@gmail.com>
2025-12-05 15:30:15 +0700

    Delete ư3a9.py

--- ư3a9.py
+++
@@ -1,7 +0,0 @@
-a = float(input("a="))
-b = float(input("b="))
-c = float(input("c="))
-if a + b > c and a + c > b and b + c > a:
-    print("3 canh tren tao thanh 1 tam giac")
-else:
-    print("3 canh tren khong tao thanh 1 tam giac")
